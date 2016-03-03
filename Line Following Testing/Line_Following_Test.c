c75be9aea4540a62af6d6b4d7c92ed7ac12be6a6
Ryan Owens <ryanvader82@gmail.com>
2016-03-03 13:28:56 -0600

    Documentation of the Line Following Program

--- Line Following Testing/Line_Following_Test.c
+++ Line Following Testing/Line_Following_Test.c
@@ -1,41 +1,30 @@
 /********************************************************************  
-Drive the Create forward (straight) for 1m at a speed of 250mm/sec 
+Follow a Line until the Create Front Bumper is pressed 
 *********************************************************************/
 #define TRUE 1
 int main()
 {	
-  printf("Drive Straight"); // announce the program	
+  printf("Line Follow."); // announce the program	
   sleep(1.0); // wait 1 second
 
   create_connect(); // Open the connection between CBC and Create	
-  // As long as the create has not driven 1m
-	//beep();
-  //while (get_create_distance(0.1) < 1000)
-  //{		
-    //create_spin_CCW(250);
-	/*while(get_create_lbump(0) == 0 && get_create_rbump(0) == 0)
-	{
-		val = analog10(0);
-		printf("analog 0 = %d\n", val);
-	}*/
-  //}
-	int ana0 = 0, ana1 = 0;
-	create_full();
-	while(get_create_lbump(0) == 0 && get_create_rbump(0) == 0){
-		ana0 = analog10(0);
-		ana1 = analog10(1);
+  int ana0 = 0, ana1 = 0; // variables to store the results of the analog sensors
+	create_full(); // We don't care about safety
+	while(get_create_lbump(0) == 0 && get_create_rbump(0) == 0){ //while the bumper is not pressed
+		ana0 = analog10(0); // left sensor
+		ana1 = analog10(1); // right sensor
 		
-		printf("analog 0: %d\n", ana0);
+		printf("analog 0: %d\n", ana0);// print results
 		printf("analog 1: %d\n", ana1);
-		if((ana0 < 200) && (ana1 > 200))
+		if((ana0 < 200) && (ana1 > 200)) // if the left sensor is off and right is on black line
 		{
-			create_spin_CW(128);
-		}else if((ana1 < 200) && (ana0 > 200))
+			create_spin_CW(128); // spin the create Clock Wise
+		}else if((ana1 < 200) && (ana0 > 200)) // else if reversed
 		{
-			create_spin_CCW(128);
-		}else if((ana0 < 200) && (ana1 < 200))
+			create_spin_CCW(128); // spin Counter Clock Wise
+		}else if((ana0 < 200) && (ana1 < 200)) // else neither is on the line
 		{
-			create_drive_straight(180);
+			create_drive_straight(180); // drive straight
 		}
 		
 	}
