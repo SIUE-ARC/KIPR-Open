ffe034a7a9be7beabaee0fd54206a7d3e89f2fdd
BryanOrabutt <friendlyhobo@gmail.com>
2016-04-01 00:41:20 -0500

    Fixed code formatting.

--- PSoC Code/KIPR_2/KIPR_2/KIPR_2/main.c
+++ PSoC Code/KIPR_2/KIPR_2/KIPR_2/main.c
@@ -23,12 +23,10 @@ signed long int count2 = 0;
 
 void main(void)
 {
-	M8C_EnableGInt ; // Uncomment this line to enable Global Interrupts
+	M8C_EnableGInt ;
 	M8C_EnableIntMask(INT_MSK0, INT_MSK0_GPIO);
 	M8C_EnableIntMask(INT_MSK1, INT_MSK1_DBB00);
 	M8C_EnableIntMask(INT_MSK1, INT_MSK1_DBB11);
-	
-	// Insert your main routine code here.
 	
 	while(1)
 	{
@@ -39,22 +37,14 @@ void main(void)
 
 void encoder1_ISR(void)
 {
-	curPrt1 = (ENC1A_Data_ADDR & (ENC1A_MASK | ENC1B_MASK));		// Setting prevPort to only bits 1[4] and 1[5]
-																// of PRT1DR
+	curPrt1 = (ENC1A_Data_ADDR & (ENC1A_MASK | ENC1B_MASK));
 		
-	if ((prevPrt1 == 0x00) && (curPrt1 == stateA1))	// If prevPort is 0x00 and then after the interrupt curPrt is
-												// 0x10 then A is high and B is low which means you wanted to
-												// increment by turning clockwise and hitting a rising edge on A
+	if ((prevPrt1 == 0x00) && (curPrt1 == stateA1))
 	{
-		// Increasing the count when clockwise turn interrupt occurred
 		count1++;
 	}
-	else if ((prevPrt1 == 0x00) && (curPrt1 == stateB1))	// If prevPort is 0x00 and then after the interrupt curPrt is
-													// 0x20 then B is high and A is low which means you wanted to
-													// decrement by turning counterclockwise and hitting 
-													// a rising edge on B
+	else if ((prevPrt1 == 0x00) && (curPrt1 == stateB1))
 	{
-		// Decreasing the count when the counterclockwise interrupt occurred
 		count1--;
 	}
 }
@@ -62,22 +52,14 @@ void encoder1_ISR(void)
 
 void encoder2_ISR(void)
 {
-	curPrt2 = (ENC2A_Data_ADDR & (ENC2A_MASK | ENC2B_MASK));		// Setting prevPort to only bits 1[4] and 1[5]
-																// of PRT1DR
+	curPrt2 = (ENC2A_Data_ADDR & (ENC2A_MASK | ENC2B_MASK));	
 		
-	if ((prevPrt2 == 0x00) && (curPrt2 == stateA2))	// If prevPort is 0x00 and then after the interrupt curPrt is
-												// 0x10 then A is high and B is low which means you wanted to
-												// increment by turning clockwise and hitting a rising edge on A
+	if ((prevPrt2 == 0x00) && (curPrt2 == stateA2))	
 	{
-		// Increasing the count when clockwise turn interrupt occurred
 		count2++;
 	}
-	else if ((prevPrt2 == 0x00) && (curPrt2 == stateB2))	// If prevPort is 0x00 and then after the interrupt curPrt is
-													// 0x20 then B is high and A is low which means you wanted to
-													// decrement by turning counterclockwise and hitting 
-													// a rising edge on B
+	else if ((prevPrt2 == 0x00) && (curPrt2 == stateB2))
 	{
-		// Decreasing the count when the counterclockwise interrupt occurred
 		count2--;
 	}
 }
