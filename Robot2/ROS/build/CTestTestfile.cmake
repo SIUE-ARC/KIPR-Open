a5349ece90694ca4b9aecf6abfc8027bd6b8de22
U-CAMPUS\borabut <borabut@EB1012-98W.cs.siue.edu>
2016-04-05 13:42:33 -0500

    Fixed PSoC code conflicts and reorganized code in repo

---
+++ Robot2/ROS/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ryanvade/build/KIPR-Open/Robot2/src
+# Build directory: /home/ryanvade/build/KIPR-Open/Robot2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
