9e108102371707064f57f58133134f736b733f8f
Ryan Owens <ryanvader82@gmail.com>
2016-04-07 17:07:25 -0500

    Added ROS packages

---
+++ Robot2/ROS/src/serial/scripts/serial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import serial, rospy, time
+from std_msgs.msg import String
+
+SERIAL_PORT = ""
+BAUD_RATE = 19200
+
+RX_TOPIC = ""
+TX_TOPIC = ""
+
+serial_connection = serial.Serial()
+
+def init_module():
+    # initialize the serial connection
+    try:
+        serial_connection.port = SERIAL_PORT
+        serial_connection.baudrate = BAUD_RATE
+        serial_connection.timeout = 1
+        serial_connection.open()
+    except serial.SerialException as e:
+        print(e)
+        exit(1)
+
+def callback(data):
+    global serial_connection
+    command, blank, command_data = str(data).partition(" ")
+    command, blank, param = str(command_data).partition(" ")
+    serial_connection.write(command + command_data)
