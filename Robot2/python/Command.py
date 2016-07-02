79aa62ca4b152108a66589923ae1149ec9703d95
Zach Anderson <zach.inbox@gmail.com>
2016-07-02 04:46:49 -0500

    Tweaked encoder turn command

--- Robot2/python/Command.py
+++ Robot2/python/Command.py
@@ -48,18 +48,51 @@ class Drive(Command):
         else:
             motor_control.mav(self.__speed_ticks * signum(error), self.__speed_ticks * signum(error))
 
+
+# Turns `angle` degrees at a rate of `speed` degrees/s where `angle` < 0 means counter clockwise.
+# Schedules `next` to be executed upon completion
 class Turn(Command):
-    def __init__(self, motor_controller, angle = 0):
+    def __init__(self, motor_controller, angle = 0, speed = 0, next = None):
         super().__init__(motor_controller)
-        self.__angle = angle
+
+        radius = self.__WHEELBASE / 2
+
+        # Linear distance a single track will travel in cm
+        wheel_arc = radius * math.radians(angle)
+
+        # Number of ticks each encoder should turn (In opposite directions)
+        self.__target_ticks = wheel_arc * self.__TICKS_PER_CM
+
+        tangent_speed = radius.math.radians(speed)
+        self.__speed_ticks = tangent_speed * self.__TICKS_PER_CM
+
+        self.__start_ticks_1
+        self.__start_ticks_2
+        self.__started = False
+        self.__next = next;
+        
 
     def execute(self):
-        radius = self.__WHEELBASE / 2
-        wheel_arc = radius * math.radians(self.__angle)
-        ticks = wheel_arc * self.__TICKS_PER_CM
+        if self.__started is False:
+            # Set start_ticks_1/2 to current ticks
+            self.__started = True
 
-        # Set motor speeds for correct direction
-        self.__motor_control.move_at_velocity()
-        if abs(getEncoder1Count()) > ticks:
-            # Do next thing
-            pass
+        delta1 = self.__current_ticks_1 - self.__start_ticks_1
+        delta2 = self.__current_ticks_2 - self.__start_ticks_2
+        avgDelta = (abs(delta1) + abs(delta2)) / 2.0
+
+        # If the right encoder has turned more than the left, we turned left
+        if delta2 > delta1:
+            avgDelta = -avgDelta
+
+        error = self.__target_ticks - avgDelta
+
+        if abs(error) < TOLERANCE:
+            return self.__next
+
+        # TODO Need to add proportinal constant?
+        else:
+            # If we need to turn left, error < 0, left motor should go backwards and right
+            # motor should go forwards
+            motor_control.mav(self.__speed_ticks * signum(error), self.__speed_ticks * -signum(error))
+
