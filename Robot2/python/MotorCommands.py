41c835ac31230a50e0cb9e8d38e62785157bc2cd
Zach Anderson <zach.inbox@gmail.com>
2016-07-02 13:00:21 -0500

    Renamed and refactored things

--- Robot2/python/MotorCommands.py
+++ Robot2/python/MotorCommands.py
@@ -1,20 +1,5 @@
-__authors__ = 'Ryan Owens Zach Anderson'
-__Creation_Date__ ='06/30/2016'
-__Last_Update__ = '07/02/2016'
-
 import math
-
-class Command:
-    def __init__(self, motor_controller=None, servo_controller=None, sensor_controller=None, debug=True):
-        self.__motor_controller = motor_controller
-        self.__servo_controller = servo_controller
-        self.__sensor_controller = sensor_controller
-        self.__DEBUG = debug
-        self.__WHEELBASE = 25
-        self.__TICKS_PER_CM = 2
-
-    def execute():
-        pass
+from Command import Command
 
 # Moves `distance` cm at a rate of `speed` cm/s. Upon completion schedules `next`
 # to be executed
