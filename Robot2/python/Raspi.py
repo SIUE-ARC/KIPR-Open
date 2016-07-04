85bd0f8107be3dd524e046db4bc51fc9045cb039
Ryan Owens <RyanOwens@linux.com>
2016-07-03 21:06:52 -0500

    Fixed elif

--- Robot2/python/Raspi.py
+++ Robot2/python/Raspi.py
@@ -16,7 +16,6 @@ class Raspi:
         self.__mode_set = []
         self.__output_pins = []
         self.__input_pins = []
-        self.__DEBUG = debug
         for i in range(1, 26):
             # If we change a pin to output/input don't allow it to be changed again
             self.__mode_set[i] = False
@@ -30,9 +29,9 @@ class Raspi:
                 self.__mode_set[pin] = True
                 self.__output_pins.append(pin)
                 GPIO.setup(pin, self.__OUT)
-            elif self.__DEBUG is True:
+            elif self._DEBUG is True:
                 print("Unable to set " +  str(pin) + " to output, it has already been set")
-        elif self.__DEBUG is True:
+        elif self._DEBUG is True:
             print(str(pin) + " is not a valid pin")
 
     def set_pin_to_input(self, pin):
@@ -43,25 +42,25 @@ class Raspi:
                 GPIO.setup(pin, self.__IN)
             elif self.__DEBUG is True:
                 print("Unable to set " +  str(pin) + " to input, it has already been set")
-        elif self.__DEBUG is True:
+        elif self._DEBUG is True:
             print(str(pin) + " is not a valid pin")
 
     def set_pin_to_high(self, pin):
         if pin in self.__output_pins:
             GPIO.output(pin, self.__HIGH)
-        elif:
+        else:
             print("Pin is not in the list of output pins.")
 
     def set_pin_to_low(self, pin):
         if pin in self.__output_pins:
             GPIO.output(pin, self.__LOW)
-        elif:
+        elif self._DEBUG = True:
             print("Pin is not in the list of output pins.")
 
     def get_pin_input(self, pin):
         if pin in self.__input_pins:
             return GPIO.input(pin)
-        elif:
+        elif self._DEBUG:
             print("Pin is not in the list of input pins.")
 
     def raspi_info(self):
